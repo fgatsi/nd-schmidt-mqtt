706174bb99a08eadfa946dfc0392e15d92e10c9d
Francis Gatsi <fgatsi@nd.edu>
2024-05-14 10:56:40 -0400

    Filtering out illegal topics

--- pi-monitor.py
+++ pi-monitor.py
@@ -9,12 +9,7 @@ from datetime import datetime, timezone
 from prettytable import PrettyTable
 
 # Topic expression using a single wildcard
-topic1 = "Schmidt/D8-3A-DD-63-C6-76/report/status"
-topic2 = "Schmidt/D8-3A-DD-5C-E1-94/report/status"
-topic3 = "Schmidt/D8-3A-DD-5C-E3-DB/report/status"
-topic4 = "Schmidt/D8-3A-DD-5C-A3-9A/report/status"
-topic5 = "Schmidt/D8-3A-DD-63-C8-5A/report/status"
-
+topic = "Schmidt/+/report/status"
 
 # Initialize a report dictionary
 report = {}
@@ -67,7 +62,7 @@ def attn_table(table, column_name):
     # Get the index of the column using the column name
     column_index = table.field_names.index(column_name)
     attn_table.field_names = table.field_names
-    attn_table.title = "ATTENTION TABLE"
+    attn_table.title = "@Francis: The following RPIs need attention."
     table.header_style = "title"
     # Loop through old table rows and filter by 'attn' column
     for row in table._rows:
@@ -141,75 +136,69 @@ def retrieve_id(mac):
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("------------Connected successfully, please wait for the results -------------")
-    client.subscribe(topic1, qos=1)
-    client.subscribe(topic2, qos=1)
-    client.subscribe(topic3, qos=1)
-    client.subscribe(topic4, qos=1)
-    client.subscribe(topic5, qos=1)
+    client.subscribe(topic, qos=1)
 
 # The Callback function to execute whenever messages are received
 def on_message(client, userdata, msg):
     global report, report_table, eth0_data, wlan0_data
+   
     try:
         pi_mac = msg.topic.split("/")[1]
-        id = retrieve_id(pi_mac)
-        #print(f'{id} : {pi_mac}')
-        # get the published msg, extract the timestamp and calculate age
-        retained_msg = json.loads(msg.payload.decode())
-        current_time = datetime.now(timezone.utc).astimezone().isoformat()
-        last_msg_time = retained_msg["timestamp"]
-        age = round(calculate_iso_difference(current_time, last_msg_time) / 60)  # in minutes
+        if len(pi_mac) == 17 and retrieve_id(pi_mac): 
+            id = retrieve_id(pi_mac)
 
-        # Get the Ethernet and Wi-Fi status
-        out_data = retained_msg['out']
-        interfaces = out_data['ifaces']
-        # Iterate through the list of interfaces
-        for interface in interfaces:
-            if interface["name"] == "eth0":
-                eth0_data = {
-                    'up': interface['up'],
-                    'ip_address': interface['ip_address'],
-                    'mac_address': interface['mac_address']
-                }
-            elif interface["name"] == "wlan0":
-                wlan0_data = {
-                    'up': interface['up'],
-                    'ip_address': interface['ip_address'],
-                    'mac_address': interface['mac_address']
-                }
+            # get the published msg, extract the timestamp and calculate age
+            retained_msg = json.loads(msg.payload.decode())
+            current_time = datetime.now(timezone.utc).astimezone().isoformat()
+            last_msg_time = retained_msg["timestamp"]
+            age = round(calculate_iso_difference(current_time, last_msg_time) / 60)  # in minutes
 
-        # Testing
-        eth_status = "UP" if eth0_data['up'] and eth0_data['ip_address'] else "DOWN"
-        wifi_status = "UP" if wlan0_data['up'] and wlan0_data['ip_address'] else "DOWN"
+            # Get the Ethernet and Wi-Fi status
+            out_data = retained_msg['out']
+            interfaces = out_data['ifaces']
+            # Iterate through the list of interfaces
+            for interface in interfaces:
+                if interface["name"] == "eth0":
+                    eth0_data = {
+                        'up': interface['up'],
+                        'ip_address': interface['ip_address'],
+                        'mac_address': interface['mac_address']
+                    }
+                elif interface["name"] == "wlan0":
+                    wlan0_data = {
+                        'up': interface['up'],
+                        'ip_address': interface['ip_address'],
+                        'mac_address': interface['mac_address']
+                    }
 
-        #if age > 120 or wifi_status == "DOWN":
-        #    attention_needed = "YES"
-        #else:
-        #    attention_needed = "NO"
-        if age > 120:
-            attention_needed = "YES"
-            eth_status = 'UNKNOWN'
-            wifi_status = 'UNKNOWN'
-        elif age < 120 and wifi_status == "DOWN":                         
-            attention_needed = 'MAYBE'
-        else:
-            attention_needed = "NO"
+            # Testing
+            eth_status = "UP" if eth0_data['up'] and eth0_data['ip_address'] else "DOWN"
+            wifi_status = "UP" if wlan0_data['up'] and wlan0_data['ip_address'] else "DOWN"
 
-        # Generate the row  corresponding to this raspberry pi
-        report["RPI_ID"] = id  # Pi_id
-        report["MAC"] = retained_msg["mac"]  # for eth0
-        report["ETH_Status"] = eth_status
-        report["WiFi_Status"] = wifi_status
+            if age > 120:
+                attention_needed = "YES"
+                eth_status = 'UNKNOWN'
+                wifi_status = 'UNKNOWN'
+            elif age < 120 and wifi_status == "DOWN":                         
+                attention_needed = 'MAYBE'
+            else:
+                attention_needed = "NO"
 
-        if age < 2:
-            report["Last_Report"] = f"{age} min ago"
-        else:
-            report["Last_Report"] = f"{age} mins ago"
+            # Generate the row  corresponding to this raspberry pi
+            report["RPI_ID"] = id  # Pi_id
+            report["MAC"] = retained_msg["mac"]  # for eth0
+            report["ETH_Status"] = eth_status
+            report["WiFi_Status"] = wifi_status
 
-        report["Attention"] = attention_needed
+            if age < 2:
+                report["Last_Report"] = f"{age} min ago"
+            else:
+                report["Last_Report"] = f"{age} mins ago"
 
-        # Add row to table
-        report_table = update_table(report)
+            report["Attention"] = attention_needed
+
+            # Add row to table
+            report_table = update_table(report)
     
     except json.decoder.JSONDecodeError as e:
         print("Error decoding JSON:", e)

7c1a0cf19d49bc83b6038f7cd980936c0a7b1d8a
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2024-06-25 14:05:40 -0500

    Shorten the device table rows by converting minutes to hours and days whenever necessary. Added number of data and data cap columns.

--- device-status.py
+++ device-status.py
@@ -98,9 +98,17 @@ def calculate_age(data):
             timestamp_str = item.get(key)
             if timestamp_str and timestamp_str != 'NaN':
                 timestamp_dt = ms_to_iso(timestamp_str)
-                age_delta = calculate_iso_difference(current_time, timestamp_dt)
-                age_minutes = int(age_delta / 60.0)
-                item[key] = f"{age_minutes} min(s) ago"
+                age_delta = calculate_iso_difference(
+                    current_time, timestamp_dt)
+                age_suffix = "mins"
+                age_str = int(age_delta / 60.0)     # minutes
+                if age_str > 120:
+                    age_str = int(age_str / 60.0)     # hours
+                    age_suffix = "hrs"
+                if age_str > 48:
+                    age_str = int(age_str / 24.0)     # days
+                    age_suffix = "days"
+                item[key] = f"{age_str} {age_suffix} ago"
             elif timestamp_str == 'NaN':
                 item[key] = 'N/A'
     return data
@@ -134,15 +142,24 @@ def main(device_file_path):
 
     # Define and print the new table with age strings
     table = PrettyTable()
-    table.field_names = ['RPI_ID', 'ONLINE', 'LAST_HB', 'LAST_TEST_ETH', 'LAST_TEST_WLAN']
+    table.field_names = ['RPI_ID', 'UP', 'LAST_HB', 'LAST_TEST_ETH',
+                         'LAST_TEST_WLAN', '#DATA', 'CAP']
     table.title = "DATA FROM FIREBASE"
     for item in updated_data:
-        if item['online']:
-            item['online'] = 'YES'
-        else:
-            item['online'] = 'NO'
+        max_num_data = max(filter(
+            lambda x: x is not None,
+            [item["day_worth_of_dl_data_eth"],
+             item["day_worth_of_dl_data_wlan"],
+             item["day_worth_of_ul_data_eth"],
+             item["day_worth_of_ul_data_wlan"]]))
         table.add_row(
-            [item['RPI_ID'], item['online'], item['last_timestamp'], item['last_test_eth'], item['last_test_wlan']])
+            [item['RPI_ID'],
+             "YES" if item["online"] else "NO",
+             item['last_timestamp'],
+             item['last_test_eth'],
+             item['last_test_wlan'],
+             f"{round(max_num_data)} days",
+             "YES" if item["data_used_gbytes"] > 100 else "NO"])
 
     table.sortby = "RPI_ID"
     print(table)
